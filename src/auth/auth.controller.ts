e02712e8e994d5b836f02283e53437024e2bd17f
jramaya <jr.amaya@yandex.com>
2024-09-05 14:28:54 -0700

    schema.prisma update for tipicos store

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,31 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UnauthorizedException } from "@nestjs/common";
-import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
-import { AuthService } from "./auth.service";
-import { LoginInput } from "./dto/login.input";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginInput } from './dto/login.input';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    @ApiOperation({ summary: 'Login' })
-    @ApiResponse({ status: 200, description: 'User logged in' })
-    @ApiResponse({ status: 401, description: 'Invalid credentials.' })
-    async login(@Body() loginInput: LoginInput) {
-        const { email, password } = loginInput;
-        const token = await this.authService.login(email, password);
-        if(!token) {
-            throw new UnauthorizedException('Invalid credentials');
-        }
-        return { accessToken: token };
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({ status: 200, description: 'User logged in' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials.' })
+  async login(@Body() loginInput: LoginInput) {
+    const { email, password } = loginInput;
+    const token = await this.authService.login(email, password);
+    if (!token) {
+      throw new UnauthorizedException('Invalid credentials');
     }
+    return { accessToken: token };
+  }
 }
