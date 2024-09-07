3364dc8c43b3bc71dc68c26909a028e387245cfc
Edgar Amaya <jr.amaya@yandex.com>
2024-09-07 19:58:39 -0400

    lint fix

--- src/products/model/product-pagination.model.ts
+++ src/products/model/product-pagination.model.ts
@@ -1,5 +1,5 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { Product } from "./product.model";
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Product } from './product.model';
 import { ApiProperty } from '@nestjs/swagger';
 
 @ObjectType()
