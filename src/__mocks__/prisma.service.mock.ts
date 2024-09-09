f02ca3234f79a3c676d2b77610915bf447a9dc44
Edgar Amaya <jr.amaya@yandex.com>
2024-09-09 00:26:43 -0400

    prisma service mock update

--- src/__mocks__/prisma.service.mock.ts
+++ src/__mocks__/prisma.service.mock.ts
@@ -1,6 +1,14 @@
 export const mockPrismaService = {
-  user: {
-    findMany: jest.fn(),
-    findUnique: jest.fn(),
+  $connect: jest.fn().mockResolvedValue(undefined),
+  $disconnect: jest.fn().mockResolvedValue(undefined),
+  $on: jest.fn(),
+  product: {
+    create: jest.fn().mockResolvedValue({}),
+    findUnique: jest.fn().mockResolvedValue(null),
+    update: jest.fn().mockResolvedValue({}),
+    delete: jest.fn().mockResolvedValue({}),
+    findMany: jest.fn().mockResolvedValue([]),
+    count: jest.fn().mockResolvedValue(0),
   },
+  onModuleInit: jest.fn().mockResolvedValue(undefined),
 };
